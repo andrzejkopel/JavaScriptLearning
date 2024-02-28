7be3761f6ec5c84c54a3c7888472c26caab017dc
andrzejkopel <andrzejkopel@gmail.com>
2024-02-28 14:50:22 +0100

    underscore added

--- JSbasics/jQuery.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-$('#button').html('New BUTTON').css('color','green');
-$('.my-unslider').unslider();
-$('li').css('color','green');
-});
---
+++ JSbasics/myscript.js
@@ -0,0 +1,8 @@
+// $(document).ready(function() {
+// $('#button').html('New BUTTON').css('color','green');
+// $('.my-unslider').unslider();
+// $('li').css('color','green');
+// });
+
+var myArray = [1, 2, 3, 4, 5, 6, 7];
+console.log(myArray);
