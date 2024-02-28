f018d9238bb269782d090a2fd12b1b3964d026c6
andrzejkopel <andrzejkopel@gmail.com>
2024-02-28 22:05:18 +0100

    lodash added

--- JSbasics/myscript.js
+++ JSbasics/myscript.js
@@ -6,12 +6,29 @@
 // });
 
 // underscore.js || _
+// var myArray = [1, 2, 3, 4, 5, 6, 7];
+// console.log(myArray);
+// var x = _.shuffle(myArray); // mix value between indexes
+// console.log(x);
+// var y = _.map(myArray, function(v){ return v * 7 }); // go thru Array
+// alert(y);
+// var z = _.reduce(myArray, function(a, b) { return a + b }) //reduce Array to one value
+// alert(z);
+// var u = _.random(0, 100) // one value random defined from range inclusive
+
+// lodash.js || _
 var myArray = [1, 2, 3, 4, 5, 6, 7];
-console.log(myArray);
-var x = _.shuffle(myArray); // mix value between indexes
+var string = 'Simple text to check';
+var a = 4.562;
+
+var x = _.drop(myArray, 3) // drop three values from index 0
 console.log(x);
-var y = _.map(myArray, function(v){ return v * 7 }); // go thru Array
-alert(y);
-var z = _.reduce(myArray, function(a, b) { return a + b }) //reduce Array to one value
-alert(z);
-var u = _.random(0, 100) // one value random defined from range inclusive
+
+var stringArray = _.split(string, ' ');
+console.log(stringArray);
+
+var k = _.sum(myArray);
+console.log(k);
+
+var m = _.ceil(a, 2); // Computes number rounded up to precision, 
+console.log(m);
